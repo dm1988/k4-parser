f2ffb73b51174ef16c201e1c465ae680ab263910
David Gonzalez <dm1988@msn.com>
2026-06-26 08:43:58 -0400

    mod: order of flightaware url

--- resources/views/parser/partials/flight-card/flight-details.blade.php
+++ resources/views/parser/partials/flight-card/flight-details.blade.php
@@ -19,27 +19,7 @@
             </p>
         </div>
     </div>
-    @if ( $model->flight->tailNumber )
-    <div>
-        <div class="flex items-center justify-between gap-3 rounded-lg bg-[#F8FAFD] px-3 py-2">
-            <p class="text-[11px] font-semibold uppercase tracking-wide text-[#8A97AB]">
-                Flight Tracking
-            </p>
-            <a href="https://flightaware.com/live/flight/{{ $model->flight->tailNumber }}" target="_blank"
-                rel="noopener noreferrer"
-                class="inline-flex min-w-0 items-center gap-1 font-mono text-sm font-semibold text-[#111827] hover:text-blue-600 group transition-colors">
-                <span class="underline decoration-transparent group-hover:decoration-blue-600 transition-all">Flight
-                    Aware</span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                    class="size-3.5 text-gray-400 group-hover:text-blue-600 transition-colors">
-                    <path fill-rule="evenodd"
-                        d="M4.25 5.5a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 .75.75v8.5a.75.75 0 0 1-1.5 0V6.56L5.28 14.03a.75.75 0 0 1-1.06-1.06L11.69 5.5H5a.75.75 0 0 1-.75-.75Z"
-                        clip-rule="evenodd" />
-                </svg>
-            </a>
-        </div>
-    </div>
-    @endif
+    
     <div class="grid grid-cols-2 gap-2 sm:col-span-2">
         <div class="flex items-center justify-between gap-3 rounded-lg bg-[#F8FAFD] px-3 py-2">
             <p class="text-[11px] font-semibold uppercase tracking-wide text-[#8A97AB]">
@@ -90,6 +70,26 @@
             </p>
         </div>
     </div>
-
+    @if ( $model->flight->tailNumber )
+    <div>
+        <div class="flex items-center justify-between gap-3 rounded-lg bg-[#F8FAFD] px-3 py-2">
+            <p class="text-[11px] font-semibold uppercase tracking-wide text-[#8A97AB]">
+                Flight Tracking
+            </p>
+            <a href="https://flightaware.com/live/flight/{{ $model->flight->tailNumber }}" target="_blank"
+                rel="noopener noreferrer"
+                class="inline-flex min-w-0 items-center gap-1 font-mono text-sm font-semibold text-[#111827] hover:text-blue-600 group transition-colors">
+                <span class="underline decoration-transparent group-hover:decoration-blue-600 transition-all">Flight
+                    Aware</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                    class="size-3.5 text-gray-400 group-hover:text-blue-600 transition-colors">
+                    <path fill-rule="evenodd"
+                        d="M4.25 5.5a.75.75 0 0 1 .75-.75h8.5a.75.75 0 0 1 .75.75v8.5a.75.75 0 0 1-1.5 0V6.56L5.28 14.03a.75.75 0 0 1-1.06-1.06L11.69 5.5H5a.75.75 0 0 1-.75-.75Z"
+                        clip-rule="evenodd" />
+                </svg>
+            </a>
+        </div>
+    </div>
+    @endif
 
 </div>
