63af4c8a0224c5a59291232552e22ef88920ac68
dm1988 <dm1988@msn.com>
2026-06-30 13:26:19 -0500

    mod: livewire for auto updating

--- app/Http/Controllers/FleetTimetableController.php
+++ app/Http/Controllers/FleetTimetableController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\View\Models\FleetTimetableViewModel;
 use Illuminate\View\View;
 
 class FleetTimetableController extends Controller
 {
     public function __invoke(): View
     {
-        return view('fleet-timetable', [
-            'viewModel' => FleetTimetableViewModel::make(),
-        ]);
+        return view('fleet-timetable');
     }
 }
