60d8fc136faf7c5341d23460b6c2c7605acf7977
David Gonzalez <dm1988@msn.com>
2026-07-22 15:44:23 -0400

    fix: duplicate alpine imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,3 @@
-import Alpine from 'alpinejs';
 import initializeFlightReleaseCopyButtons from './flight-release-copy';
-
-window.Alpine = Alpine;
-
-Alpine.start();
 
 initializeFlightReleaseCopyButtons();
